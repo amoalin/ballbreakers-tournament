a63c17c00cf2c507e225e0a5119dc34b96a3deff
andres <amoalin@mail.uniatlantico.edu.co>
2026-05-28 11:10:23 -0500

    agregando diseño y grupos

--- public/app.js
+++ public/app.js
@@ -1,15 +1,24 @@
-async function cargarEquipos() {
+let equipos = [];
+
+async function cargarEquipos(){
 
   const res = await fetch("/api/equipos");
-  const equipos = await res.json();
+
+  equipos = await res.json();
+
+  mostrarEquipos();
+}
+
+function mostrarEquipos(){
 
   const lista = document.getElementById("listaEquipos");
 
   lista.innerHTML = "";
 
-  equipos.forEach((equipo) => {
+  equipos.forEach((equipo)=>{
 
     const li = document.createElement("li");
+
     li.textContent = equipo;
 
     lista.appendChild(li);
@@ -17,7 +26,7 @@ async function cargarEquipos() {
   });
 }
 
-async function agregarEquipo() {
+async function agregarEquipo(){
 
   const input = document.getElementById("equipoInput");
 
@@ -28,12 +37,12 @@ async function agregarEquipo() {
     return;
   }
 
-  await fetch("/api/equipos", {
+  await fetch("/api/equipos",{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
     },
-    body: JSON.stringify({
+    body:JSON.stringify({
       nombre
     })
   });
@@ -41,6 +50,57 @@ async function agregarEquipo() {
   input.value = "";
 
   cargarEquipos();
+}
+
+function generarGrupos(){
+
+  const gruposContainer =
+    document.getElementById("gruposContainer");
+
+  gruposContainer.innerHTML = "";
+
+  if(equipos.length < 4){
+    alert("Necesitas mínimo 4 equipos");
+    return;
+  }
+
+  let mezcla = [...equipos];
+
+  mezcla.sort(()=>Math.random()-0.5);
+
+  const grupos = {
+    A:[],
+    B:[]
+  };
+
+  mezcla.forEach((equipo,index)=>{
+
+    if(index % 2 === 0){
+      grupos.A.push(equipo);
+    }else{
+      grupos.B.push(equipo);
+    }
+
+  });
+
+  for(let letra in grupos){
+
+    const div = document.createElement("div");
+
+    div.classList.add("grupo");
+
+    div.innerHTML = `
+      <h3>Grupo ${letra}</h3>
+      <ul>
+        ${grupos[letra]
+          .map(eq=>`<li>${eq}</li>`)
+          .join("")}
+      </ul>
+    `;
+
+    gruposContainer.appendChild(div);
+
+  }
 }
 
 cargarEquipos();
